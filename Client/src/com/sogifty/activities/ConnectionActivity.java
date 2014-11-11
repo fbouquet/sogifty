6c81ec0a349f7a1ab2e81bee4d533400ff945385
lvillalba <leo.villalba@hotmail.fr>
2014-11-11 17:12:03 +0100

    Ajout activity connect

---
+++ Client/src/com/sogifty/activities/ConnectionActivity.java
@@ -0,0 +1,56 @@
+package com.sogifty.activities;
+
+
+import com.sogifty.R;
+import com.sogifty.model.User;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ConnectionActivity extends Activity{
+	
+
+	Button connectButton;
+	EditText emailText;
+	EditText passwordText;
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_connection);
+		initializeActivity();
+		setButtonListener();
+	}
+	
+	private void initializeActivity() {
+		connectButton = (Button) findViewById(R.id.connection_b_connection);
+		emailText = (EditText) findViewById(R.id.connection_et_email);
+		passwordText = (EditText) findViewById(R.id.connection_et_password);			
+	}
+	
+	
+	private void setButtonListener() {
+		connectButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				createFriendListActivty();				
+			}
+		});
+	}
+	
+	protected void createFriendListActivty() {
+		Intent intent = HelloAndroidActivity.getIntent(this, emailText.getText().toString(), passwordText.getText().toString());
+		startActivity(intent);
+	}
+
+}
