76fb6cce26f8b0749f3b477b0f095476dcdc344f
ajomard <ajomard@enseirb-matmeca.fr>
2014-11-28 11:43:46 +0100

    Tente de merge

--- Client/src/com/sogifty/activities/Setting.java
+++
@@ -1,8 +0,0 @@
-package com.sogifty.activities;
-
-public class Setting {
-
-	public static String UserID = "";
-	public  static String UserLastName = "";
-	public  static String UserFirstName = "";
-}
